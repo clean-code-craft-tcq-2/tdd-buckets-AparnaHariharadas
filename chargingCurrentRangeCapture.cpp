9b353a68680a9eed7f4e6dba1ece2669339c5d46
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-08 18:21:41 +0530

    Update chargingCurrentRangeCapture.cpp

--- chargingCurrentRangeCapture.cpp
+++ chargingCurrentRangeCapture.cpp
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <sstream> // for ostringstream
 #include <string>
-#include "InterpretChargingCurrentRangeAndOccurences.h"
 
 char* captureChargingCurrentRange(int* chargingCurrentSamples){
 	char *chargingCurrentRangeAndOccurences = (char*)malloc(100);
