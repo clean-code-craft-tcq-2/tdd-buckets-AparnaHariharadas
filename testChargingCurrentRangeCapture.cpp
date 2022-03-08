d16e28ee0aeb45f5e95e7b7534e910ba94edda34
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-08 19:05:01 +0530

    Update testChargingCurrentRangeCapture.cpp

--- testChargingCurrentRangeCapture.cpp
+++ testChargingCurrentRangeCapture.cpp
@@ -8,5 +8,8 @@ TEST_CASE("Checks the charging current ranges and captures the no of occurences"
   REQUIRE(captureChargingCurrentRange(CurrentRanges) == 1);
   int CurrentRangesnew[6] = {3,3,4,4,5,6};
   REQUIRE(captureChargingCurrentRange(CurrentRangesnew) == 5);
+   int CurrentRangeslatest[6] = {3,4,5,6,7,8};
+  REQUIRE(captureChargingCurrentRange(CurrentRangeslatest) == 5);
+}
 }
 
