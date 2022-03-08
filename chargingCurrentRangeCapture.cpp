a554c19d695381b3fe686be3944dcdcaa6411a96
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-08 18:37:25 +0530

    Update chargingCurrentRangeCapture.cpp

--- chargingCurrentRangeCapture.cpp
+++ chargingCurrentRangeCapture.cpp
@@ -33,22 +33,30 @@ int findMaxValueOfChargingCurrentRange(int* chargingCurrentSamples, size_t numbe
 	return maxValue;
 }
 
-char* captureChargingCurrentRange(int* chargingCurrentSamples){
+int captureChargingCurrentRange(int* chargingCurrentSamples){
 	char *chargingCurrentRangeAndOccurences = (char*)malloc(100);
-	int maxValue, minValue;
-	size_t numberOfSamples, numberOfOccurences;
+	int maxValue, minValue, DifferenceBetweenSamples;
+	size_t numberOfSamples;
+	int numberOfOccurences = 0;
 
 	numberOfSamples = findNumberOfSamples(chargingCurrentSamples);
 	minValue = findMinValueOfChargingCurrentRange(chargingCurrentSamples, numberOfSamples);
 	maxValue = findMaxValueOfChargingCurrentRange(chargingCurrentSamples, numberOfSamples);
-	numberOfOccurences = numberOfSamples; // Since input is conidered as a single range
+	for (LoopIndex = 0; LoopIndex<numberOfSamples ; LoopIndex++) {
+		//cout << CurrentSamples[LoopIndex] <<endl ;
+		DifferenceBetweenSamples = chargingCurrentSamples[LoopIndex + 1] - chargingCurrentSamples[LoopIndex];
+		if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1))
+			{
+			numberOfOccurences ++;
+			}
+	}
   std::ostringstream out;  
   out << minValue << maxValue << numberOfOccurences;
   cout << out.str() << endl;
 	//sprintf(chargingCurrentRangeAndOccurences, "%d-%d, %lu", minValue, maxValue, numberOfOccurences);
 	//cout << chargingCurrentRangeAndOccurences <<endl;//printf("Range, Readings \n");
 	//printf("%s\n", chargingCurrentRangeAndOccurences);
-	return chargingCurrentRangeAndOccurences;
+	return numberOfOccurences;
 }
 
 
