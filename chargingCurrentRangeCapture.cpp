0419312cf1f1a814f69c8d73a3a2ed40447cd7e6
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-08 14:02:07 +0530

    Create chargingCurrentRangeCapture.cpp

---
+++ chargingCurrentRangeCapture.cpp
@@ -0,0 +1,37 @@
+#include "chargingCurrentRangeCapture.h"
+#iclude <iostream.h>
+using namespace std;
+
+int captureChargingCurrentRange (int *CurrentSamples){
+  int NumOfCurrentSamples;
+  int NumOfConsecutiveRange=0;
+  int DifferenceBetweenSamples;
+  int LoopIndex;
+  int rangeMinValue=0;
+  int rangeMaxValue = 0;
+  NumOfCurrentSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+  int final_array[NumOfCurrentSamples] = {0};
+  sort(*CurrentSamples, *(CurrentSamples+NumOfCurrentSamples)); 
+  for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
+    DifferenceBetweenSamples = CurrentSamples[LoopIndex + 1] - CurrentSamples[LoopIndex];
+    if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1))
+    {
+      NumOfConsecutiveRange ++;
+      final_array[NumOfConsecutiveRange] = CurrentSamples[LoopIndex];
+    }
+    else
+    {
+      final_array[NumOfConsecutiveRange] = 0;
+      NumOfConsecutiveRange = 0;
+    }
+  }
+  rangeMinValue = final_array[0];
+  for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
+    if final_array[LoopIndex] == 0
+    {
+      rangeMaxValue = final_array[LoopIndex - 1];
+    }
+    
+  }
+       return (NumOfConsecutiveRange, rangeMinValue rangeMaxValue);
+}
