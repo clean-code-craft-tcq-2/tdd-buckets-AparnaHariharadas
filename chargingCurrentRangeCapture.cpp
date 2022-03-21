4b4104023dd104f1689769291717937d67555cc4
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-21 14:32:35 +0530

    Update chargingCurrentRangeCapture.cpp

--- chargingCurrentRangeCapture.cpp
+++ chargingCurrentRangeCapture.cpp
@@ -5,62 +5,67 @@
 #include <sstream> // for ostringstream
 #include <string>
 #include<array> 
-
+int numberOfOccurences[5], minValue[5], maxValue[5] = {0};
+int counter= 0;
 int findMinValueOfChargingCurrentRange(int* chargingCurrentSamples, int numberOfSamples){
-	int minValue;
 	minValue = chargingCurrentSamples[numberOfSamples-1]; //Initializing to last element  
 	for (int i=0; i<numberOfSamples; i++) {
 		if(chargingCurrentSamples[i] < minValue){
-			minValue = chargingCurrentSamples[i];
+			minValue[counter] = chargingCurrentSamples[i];
 		}
 	}
 	return minValue;
 }
 
 int findMaxValueOfChargingCurrentRange(int* chargingCurrentSamples, int numberOfSamples){
-	int maxValue;
 	maxValue = chargingCurrentSamples[0]; // Initializing to first element
 	for (int i=0; i<numberOfSamples; i++) {
 		if(chargingCurrentSamples[i] > maxValue){
-			maxValue = chargingCurrentSamples[i];
+			maxValue[counter] = chargingCurrentSamples[i];
 		}
 	}
 	return maxValue;
 }
 
-int checkIfConcurrent(int DifferenceBetweenSamples){
+void checkIfConcurrent(int DifferenceBetweenSamples, int currentmaxValue, int nextMinValue){
+        numberOfOccurences = numberOfOccurences + checkIfConcurrent(DifferenceBetweenSamples);
 	if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1))
-		return 1;
+		numberOfOccurences[counter] = numberOfOccurences[counter] + 1;
 			
-	else
-		return 0;
+	else{
+		counter = counter + 1;
+		maxValue[counter] = currentmaxValue;
+	        minValueNext[counter] = nextMinValue;
+		numberOfOccurences[counter] = 0;
+	}
 }
 
 
-int findNumberOfOccurences(int chargingCurrentSamples[], int numberOfSamples){
-	int DifferenceBetweenSamples, numberOfOccurences = 0;
+void findNumberOfOccurences(int chargingCurrentSamples[], int numberOfSamples){
 	for (int LoopIndex = 0; LoopIndex < numberOfSamples ; LoopIndex++) {
 		DifferenceBetweenSamples = chargingCurrentSamples[LoopIndex + 1] - chargingCurrentSamples[LoopIndex];
-		numberOfOccurences = numberOfOccurences + checkIfConcurrent(DifferenceBetweenSamples);
+		checkIfConcurrent(DifferenceBetweenSamples, chargingCurrentSamples[LoopIndex], chargingCurrentSamples[LoopIndex + 1]);
 	}
-	return numberOfOccurences;
+	
 }
 
 
 int captureChargingCurrentRange(int chargingCurrentSamples[], int noOfCurrentReadings){
 	int maxValue, minValue;
 	size_t numberOfSamples;
-	int numberOfOccurences = 0;
+	int numberOfOccurencesFinal = 0;
 
 	numberOfSamples = noOfCurrentReadings;
 	cout << numberOfSamples <<endl ;
 	minValue = findMinValueOfChargingCurrentRange(chargingCurrentSamples, numberOfSamples);
 	maxValue = findMaxValueOfChargingCurrentRange(chargingCurrentSamples, numberOfSamples);
-	numberOfOccurences = findNumberOfOccurences(chargingCurrentSamples, numberOfSamples);
+	findNumberOfOccurences(chargingCurrentSamples, numberOfSamples);
 	
-        std::ostringstream currentRangeAndOccurance;  
-        currentRangeAndOccurance << minValue << "-" << maxValue << "," << numberOfOccurences;
+        std::ostringstream currentRangeAndOccurance;
+	for (int LoopIndex = 0; LoopIndex < 5 ; LoopIndex++) {
+        currentRangeAndOccurance << minValue[LoopIndex] << "-" << maxValue[LoopIndex] << "," << numberOfOccurences[LoopIndex];
         cout << currentRangeAndOccurance.str() << endl;
+	}
         return numberOfOccurences;
 }
 
