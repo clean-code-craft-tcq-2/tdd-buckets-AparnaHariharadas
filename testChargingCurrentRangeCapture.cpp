18a71ac2ef3feabeefe1a8365e0533e30acb4cb4
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-28 12:40:57 +0530

    Update testChargingCurrentRangeCapture.cpp

--- testChargingCurrentRangeCapture.cpp
+++ testChargingCurrentRangeCapture.cpp
@@ -18,18 +18,18 @@ TEST_CASE("Checks the charging current ranges and captures the no of occurences"
 TEST_CASE("Checks 12 bit ADcinput") {
   int CurrentRanges[] = {600,800,950,1000,1200};
   int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
-  int* ptr =convertAndCheck12BitAdcValues(CurrentRanges, noOfcurrentReadings);
+  int* ptr =convertAndCheckAdcValues(CurrentRanges, noOfcurrentReadings);
   REQUIRE(ptr[0]==1);
   int CurrentRangesnew[] = {600,800,950,1000,1200,5000};
   noOfcurrentReadings = sizeof(CurrentRangesnew)/sizeof(CurrentRangesnew[0]);
-  REQUIRE(convertAndCheck12BitAdcValues(CurrentRangesnew, noOfcurrentReadings) == NULL);
+  REQUIRE(convertAndCheckAdcValues(CurrentRangesnew, noOfcurrentReadings) == NULL);
  
 }
 
 TEST_CASE("Checks 12 bit ADcinput and find ADC range") {
   int CurrentRanges[] = {600,800,950,1000,1200};
   int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
-  REQUIRE(captureConcurrent12BitADCRanges(CurrentRanges, noOfcurrentReadings) == 1);
+  REQUIRE(captureConcurrentADCRanges(CurrentRanges, noOfcurrentReadings) == 1);
 }
 
 /*TEST_CASE("Checks 10 bit ADcinput") {
