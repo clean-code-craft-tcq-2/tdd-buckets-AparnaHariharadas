8ee5c1d066f0211f3875906875ebbd328ec6a9a4
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-28 11:59:56 +0530

    Update testChargingCurrentRangeCapture.cpp

--- testChargingCurrentRangeCapture.cpp
+++ testChargingCurrentRangeCapture.cpp
@@ -18,16 +18,32 @@ TEST_CASE("Checks the charging current ranges and captures the no of occurences"
 TEST_CASE("Checks 12 bit ADcinput") {
   int CurrentRanges[] = {600,800,950,1000,1200};
   int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
-  int* ptr =convertAndCheckAdcValues(CurrentRanges, noOfcurrentReadings);
+  int* ptr =convertAndCheck12BitAdcValues(CurrentRanges, noOfcurrentReadings);
   REQUIRE(ptr[0]==1);
   int CurrentRangesnew[] = {600,800,950,1000,1200,5000};
   noOfcurrentReadings = sizeof(CurrentRangesnew)/sizeof(CurrentRangesnew[0]);
-  REQUIRE(convertAndCheckAdcValues(CurrentRangesnew, noOfcurrentReadings) == NULL);
+  REQUIRE(convertAndCheck12BitAdcValues(CurrentRangesnew, noOfcurrentReadings) == NULL);
  
 }
 
 TEST_CASE("Checks 12 bit ADcinput and find ADC range") {
   int CurrentRanges[] = {600,800,950,1000,1200};
   int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
-  REQUIRE(captureConcurrentADCRanges(CurrentRanges, noOfcurrentReadings) == 1);
+  REQUIRE(captureConcurrent12BitADCRanges(CurrentRanges, noOfcurrentReadings) == 1);
+}
+TEST_CASE("Checks 10 bit ADcinput") {
+  int CurrentRanges[] = {0,511,1022};
+  int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
+  int* ptr =convertAndCheck10BitAdcValues(CurrentRanges, noOfcurrentReadings);
+  REQUIRE(ptr[0]==0);
+  int CurrentRangesnew[] = {0,511,2000};
+  noOfcurrentReadings = sizeof(CurrentRangesnew)/sizeof(CurrentRangesnew[0]);
+  REQUIRE(convertAndCheck10BitAdcValues(CurrentRangesnew, noOfcurrentReadings) == NULL);
+ 
+}
+
+TEST_CASE("Checks 10 bit ADcinput and find ADC range") {
+  int CurrentRanges[] = {0,511,1022};
+  int noOfcurrentReadings = sizeof(CurrentRanges)/sizeof(CurrentRanges[0]);
+  REQUIRE(captureConcurrent10BitADCRanges(CurrentRanges, noOfcurrentReadings) == 1);
 }
