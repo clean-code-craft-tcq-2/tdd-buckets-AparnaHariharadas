d4a5dfb6448567f0250abdc9a9fdacb10c3580c3
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-03-28 12:47:41 +0530

    Update chargingCurrentRangeCapture.h

--- chargingCurrentRangeCapture.h
+++ chargingCurrentRangeCapture.h
@@ -3,7 +3,7 @@
 using namespace std;
 
 int captureChargingCurrentRange (int CurrentSamples[], int noOfCurrentReadings);
-int* convertAndCheckAdcValues(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings);
-//signed int* convertAndCheck10BitAdcValues(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings);
+int* convertAndCheckTwelveBitAdcValues(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings);
+signed int* convertAndCheckTenBitAdcValues(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings);
 int captureConcurrentADCRanges(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings);
 //int captureConcurrent10BitADCRanges(int chargingAdcCurrentSamples[], int noOfAdcCurrentReadings)
